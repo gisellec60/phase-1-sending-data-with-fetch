78e6c2f3162ec0619c1274d05e8c0a76c91dfb06
Giselle Smith <gisellec60@gmail.com>
2023-04-13 12:08:54 -0400

    fetching lab

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Add your code here
+function submitData(name, email) {
+   return fetch('http://localhost:3000/users', {
+            method: "POST",
+            headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+            },
+            body: JSON.stringify({
+               name: name,
+               email:email,
+            }), 
+         })
+      .then(function (response) {
+            return response.json()
+         })
+      .then(function (object) {
+         document.body.innerHTML = object.id
+      })
+      .catch(function (error) {
+         alert("Oh oh, Bad things")
+         console.log(error.message);
+         const p = document.createElement('p')
+         p.innerHTML = error.message
+         document.body.appendChild(p)
+      });
+}
